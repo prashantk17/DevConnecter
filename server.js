0f3f4f8347c154cf48d5102392ac3eb4dbb13c36
Prashant Katoch <prashantkatoch56@gmail.com>
2025-08-11 22:43:02 +0530

    Initial commit

--- server.js
+++ server.js
@@ -52,5 +52,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000
+;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
